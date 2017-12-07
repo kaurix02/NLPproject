032c9b10748ea076045e474921301487616f5510
Unknown <kaurix02@hotmail.com>
2017-12-07 21:10:41 +0200

    update xml script

--- nmt/XML_parsing.py
+++ nmt/XML_parsing.py
@@ -1,6 +1,5 @@
 
 # coding: utf-8
-
 # In[18]:
 
 
@@ -8,7 +7,7 @@ import gzip
 import xml.etree.ElementTree as ET
 import re
 import os
-
+import sys
 
 fromDoc = []
 toDoc = []
@@ -16,9 +15,16 @@ target_list = list()
 
 directory_to = ''
 diectory_from = ''
+langto = sys.argv[1]	#"ka" for Georgian etc. READ FROM COMMAND LINE
+
+#Change for your setup:
+alignfiledir = "data/"
+alignfile = alignfiledir+"en-"+langto+".xml.gz"	#alignment file
+from_f = "data/en"+langto+".en"	#Saves from-language lines
+to_f = "data/en"+langto+"."+langto	#Saves to-language lines
 
 
-with gzip.open('en-ka.xml.gz', 'rb') as f:
+with gzip.open(alignfile, 'rb') as f:
     tree = ET.parse(f)
     root = tree.getroot()
     #file_content = f.read()
@@ -26,9 +32,6 @@ with gzip.open('en-ka.xml.gz', 'rb') as f:
 
 
 #root.findall()
-
-
-
 
 for child in root:
     fromDoc.append(child.attrib['fromDoc'])
@@ -42,16 +45,18 @@ for child in root:
 
 #print target_list[0]   
 
+from_file = open(from_f, 'w')
+to_file = open(to_f, 'w')
 
 for i  in range (len(fromDoc)):
     #Directories of input
-    from_path = '/home/aghabayli/Documents/NLP/Project/en/OpenSubtitles2018/xml/' + fromDoc[i]
-    to_path = '/home/aghabayli/Documents/NLP/Project/OpenSubtitles2018/xml/' + toDoc[i]
+    from_path = 'data/' + fromDoc[i]
+    to_path = 'data/' + toDoc[i]
     #print 324
     
     #Directories of output
-    from_file = open('From_dir' + "_".join(fromDoc[i].split('/'))+'.txt', 'w')
-    to_file = open('To_dir' + "_".join(toDoc[i].split('/')) + '.txt', 'w')
+    
+
     with gzip.open(from_path, 'rb') as f:
         tree_from = ET.parse(f, ET.XMLParser(encoding='utf-8'))
         root_from = tree_from.getroot()
@@ -63,9 +68,6 @@ for i  in range (len(fromDoc)):
         #print 45
         #print j[1]
         #root_from.find("[@id = '{0}']".format(j[0]))
-        
-
-        
         for i in j[1].split():
             to_sent = root_to.findall(".//s[@id='{0}']/w".format(i))
             s = ''
@@ -85,5 +87,7 @@ for i  in range (len(fromDoc)):
             s = re.sub(r'\s+([?.!",\'])', r'\1', s)
             from_file.write(s.encode('utf-8'))
         from_file.write("\n")
+from_file.close()
+to_file.close()
         
 
